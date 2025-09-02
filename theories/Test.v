44e9d006f14a2e826fe2cdf758e1c9fa3df289ce
MathisBD <mathis.bouverot@ens.psl.eu>
2025-09-02 16:40:37 +0200

    hello world plugin

---
+++ theories/Test.v
@@ -0,0 +1,8 @@
+Declare ML Module "meta-programming-experiments.plugin".
+
+Inductive M : Type -> Type :=
+| Ret {A} : A -> M A
+| Bind {A B} : M A -> (A -> M B) -> M B
+| FreshNat : unit -> M nat.
+
+Test.
